123c54b785ce41e8ade282d3ebbb27c7f24b15c0
Dima Mazur <mazur.dmi@gmail.com>
2020-06-01 18:09:31 +0300

    chess-js v. 0.1

--- chess-js/chess.js
+++ chess-js/chess.js
@@ -1,8 +1,12 @@
+var content = document.getElementById('content');
+
 for (var i = 1; i <= 8; i++) {
 var newRow = document.createElement("div");
 newRow.setAttribute("class", "row");
     for(var j = 1; j <= 8; j++) {
 var newCell = document.createElement("div");
 newCell.setAttribute("class", "cell");
-    }
+    newRow.appendChild(newCell);
+}
+    content.appendChild(newRow);
 }
