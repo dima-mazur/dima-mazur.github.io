455c0506e70abbd06f121026d85f2f9df3a29bb6
Dima Mazur <mazur.dmi@gmail.com>
2020-06-08 12:51:32 +0300

    currency v 1.1

--- currency/main.js
+++ currency/main.js
@@ -2,8 +2,18 @@ document.getElementById("btnGet").addEventListener("click", getRate);
 
 function getRate(e) {
     var BASE_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=";
+    var radio = document.getElementsByName("inlineRadioOptions");
+    for (let i=0; i < radio.length; i++) {
+        if (radio[i].checked) {
+            var radioValue = radio[i];
+        }
+    }
+    // console.log(radioValue);
+    // document.getElementById('cdate').value = new Date().toDateInputValue
+
+    // console.log(cdate.value);
     var datev = cdate.value.split("-").join("");
-    var URI = BASE_URL + currency.value + "&date=" + datev + "&json";
+    var URI = BASE_URL + radioValue.value + "&date=" + datev + "&json";
     // console.log(URI);
     
     var XHR = new XMLHttpRequest();
@@ -16,9 +26,21 @@ function handler(e) {
     if ((e.target.readyState === 4) && (e.target.status === 200)) {
         var data = JSON.parse(e.target.responseText)[0];
         // console.log(data);
-        var result = document.createElement("span");
-        result.innerText = data.rate;
+        // var result = document.createElement("span
+        currencyValue.innerHTML = "<span>" + data.rate + "</span>";
+
+
+        // result.innerText = data.rate;
+        
         // console.log(result);
-        document.getElementById("currencyValue").appendChild(result);
+        // document.getElementById("currencyValue").appendChild(result);
+        
+        }
     }
-}
+// радиобатонн без иконок (выдает много ошибок jquery в консоле)
+    $( function() {
+        $( "input" ).checkboxradio({
+          icon: false
+        });
+      } );
+    
